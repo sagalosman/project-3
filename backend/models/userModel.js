d239b2636308bdc0843c886207053fe2cf99a591
mitty009 <mitchell.thomas808@gmail.com>
2020-11-13 19:22:12 +0300

    test

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   photo: { type: String}
+  sdlfhlaksdfjadsklfj
 })
 
 //Plugin modules for mongoose for login/registering functionality
