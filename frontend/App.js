7607c4f6a56053c9a21cc1e62bc2b5710b7a2956
sagalosman <sagalosman0@gmail.com>
2020-11-16 12:36:15 +0000

    test

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import './styles/style.scss'
+
+
+import Register from './components/register'
+import Login from './components/login'
 
 // For environment varibles
 console.log(process.env.hello)
 
-const App = () => {
-  return <h1>Hello friends</h1>
-}
+const App = () => (
+  <BrowserRouter>
+  <Navbar />
+    <Switch>
+      {/* <Route exact path="/" component={Home}/> */}
+      <Route exact path="/register" component={Register}/>
+      <Route exact path="/login" component={Login}/>
+    </Switch>
+  </BrowserRouter>
+)
+
+// const App = () => {
+//   return <h1>Hello friends</h1>
+// }
 
 export default App
