ea0bdb32f3b44ac613d9cca9559d8ea9ac69f711
mitty009 <mitchell.thomas808@gmail.com>
2020-11-18 13:29:28 +0300

    Fully merged

--- frontend/src/components/ViewProfile.js
+++ frontend/src/components/ViewProfile.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 // import NavBar from './NavBar'
 
 const ViewProfile = (props) => {
-  const userId = '5fb4ea4c7c6faf3ab025ca6c'
+  const userId = '5fb4ea4b7c6faf3ab025ca68'
   const [viewProfile, updateViewProfile] = useState({})
   const [viewEvents, updateViewEvents] = useState([])
 
@@ -47,60 +47,58 @@ const ViewProfile = (props) => {
       <h1>Loading</h1>
     </div>
   } else {
-    return <h1>Hello</h1>
+    return <section className="flex-container">
+      <div className="profile-header">
+        <div className="flex-column flex-1">
+          <p>Photo</p>
+        </div>
+        <div className="flex-column flex-2">
+          <div id="first-row" className="flex-row">
+            <div className="flex-column">
+              {/* <h4>{viewProfile.events.length}</h4> */}
+              <h6>Events</h6>
+            </div>
+            <div className="flex-column">
+              {/* <h4>{viewProfile.recentEvents.length}</h4> */}
+              <h6>Recent</h6>
+            </div>
+            <div className="flex-column">
+              {/* <h4>{viewProfile.taggedEvents.length}</h4> */}
+              <h6>Tagged</h6>
+            </div>
+          </div>
+          <div id="second-row" className="flex-row">
+            <div className="flex-column">
+              {/* <h4>{viewProfile.friends.length}</h4> */}
+              <h6>Friends</h6>
+            </div>
+            <div className="flex-column">
+              <h4>{viewProfile.topFriends.length}</h4>
+              <h6>Top Friends</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="bio">
+        <h5>{viewProfile.user.firstname} {viewProfile.user.lastname}</h5>
+        <h6>{viewProfile.bio}</h6>
+      </div>
+      <div id="add-friend">
+          <button onClick={addFriend}>Friend</button>
+          <button onClick={addTopFriend}>Add Top Friend</button>
+      </div>
+      <article id="events">
+        <div className="flex-row">
+          <div><h3>Events</h3></div>
+          {/* {viewEvents.events.map((event, index) => {
+            return <div key={index}>
+              <h1>{event.eventName}</h1>
+            </div>
+          })} */}
+        </div>
+      </article>
+    </section>
   }
-  //   return <section className="flex-container">
-  //     <div className="profile-header">
-  //       <div className="flex-column flex-1">
-  //         <p>Photo</p>
-  //       </div>
-  //       <div className="flex-column flex-2">
-  //         <div id="first-row" className="flex-row">
-  //           <div className="flex-column">
-  //             <h4>{viewProfile.events.length}</h4>
-  //             <h6>Events</h6>
-  //           </div>
-  //           <div className="flex-column">
-  //             <h4>{viewProfile.recentEvents.length}</h4>
-  //             <h6>Recent</h6>
-  //           </div>
-  //           <div className="flex-column">
-  //             <h4>{viewProfile.taggedEvents.length}</h4>
-  //             <h6>Tagged</h6>
-  //           </div>
-  //         </div>
-  //         <div id="second-row" className="flex-row">
-  //           <div className="flex-column">
-  //             <h4>{viewProfile.friends.length}</h4>
-  //             <h6>Friends</h6>
-  //           </div>
-  //           <div className="flex-column">
-  //             <h4>{viewProfile.topFriends.length}</h4>
-  //             <h6>Top Friends</h6>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <div id="bio">
-  //       <h5>{viewProfile.user.firstname} {viewProfile.user.lastname}</h5>
-  //       <h6>{viewProfile.bio}</h6>
-  //     </div>
-  //     <div id="add-friend">
-  //         <button onClick={addFriend}>Friend</button>
-  //         <button onClick={addTopFriend}>Add Top Friend</button>
-  //     </div>
-  //     <article id="events">
-  //       <div className="flex-row">
-  //         <div><h3>Events</h3></div>
-  //         {/* {viewEvents.events.map((event, index) => {
-  //           return <div key={index}>
-  //             <h1>{event.eventName}</h1>
-  //           </div>
-  //         })} */}
-  //       </div>
-  //     </article>
-  //   </section>
-  // }
 }
 
 
