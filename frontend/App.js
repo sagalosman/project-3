8bd76869e03503e9c80febf7c6084d8a9cf1350b
sagalosman <sagalosman0@gmail.com>
2020-11-19 13:42:43 +0000

    test

--- frontend/App.js
+++ frontend/App.js
@@ -16,7 +16,7 @@ const App = () => {
   return <BrowserRouter>
     <Switch>
       <Route exact path="/register" component={Register} />
-      <ProtectedRoute exact path="/profile/:userId" component={ViewProfile} />
+      <ProtectedRoute exact path="/profile/users/:userId" component={ViewProfile} />
       <ProtectedRoute exact path="/profile" component={Profile} />
       <ProtectedRoute exact path="/editprofile/:userId" component={EditProfile}/>
       <Route exact path="/" component={Login} />
